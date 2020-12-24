f3b9ca23876d52c456ffe01d89b8cadc0dcff074
Liu Ziang <39642528+AceSix@users.noreply.github.com>
2020-12-24 10:05:13 +0800

    remove different versions of mapping

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 ###
- # @FilePath: \ViT\run.sh
+ # @FilePath: \ViT-Unofficial\run.sh
  # @Author: Ziang Liu
  # @Date: 2020-12-23 17:03:30
  # @LastEditors: Ziang Liu
- # @LastEditTime: 2020-12-23 17:14:07
+ # @LastEditTime: 2020-12-24 10:01:44
  # @Copyright (C) 2020 SJTU. All rights reserved.
 ### 
 
-python main.py --version ViT1\
+python main.py --version "ViT2"\
                --description "imagenet pretrain" \
+               --image_size 384 \
+               --patch_size 12 \
                --dim 256 \
                --depth 4 \
                --train_folder /home/gdp/harddisk/Data1/Imagenet2012/train \
